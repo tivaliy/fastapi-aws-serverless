4767f562a24d883cfcc5973efcc12c4838fe75d9
Vitalii Kulanov <vkulanov@waverleysoftware.com>
2022-01-13 12:55:30 +0200

    Make basic auth scope extendable to create different auth scopes

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 
 from fastapi import Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
@@ -8,17 +8,23 @@ from app.core.config import get_app_settings
 from app.core.settings.app import AppSettings
 
 
-async def admin_scoped_auth(
+async def auth(
     settings: AppSettings = Depends(get_app_settings),
-    http_auth: Optional[HTTPAuthorizationCredentials] = Depends(HTTPBearer(auto_error=False)),
 ) -> Cognito:
-    auth = Cognito(
+    cognito_auth = Cognito(
         region=settings.aws_region,
         userPoolId=settings.userpool_id,
         client_id=settings.app_client_id,
     )
-    auth.scope_name = ["admins"]
-    return await auth(http_auth)
+    return cognito_auth
+
+
+async def admin_scoped_auth(
+    cognito_auth: auth = Depends(),
+    http_auth: Optional[HTTPAuthorizationCredentials] = Depends(HTTPBearer(auto_error=False)),
+) -> List[str]:
+    cognito_auth.scope_name = ["admins"]
+    return await cognito_auth(http_auth)
 
 
 async def get_current_user(
